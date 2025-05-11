701c4180b986c656ce455529dbebec77debae518
Xavier Chen <xaviernehc@gmail.com>
2025-05-11 17:14:35 +0800

    feat: add initial flask config py file

---
+++ config.py
@@ -0,0 +1,37 @@
+from typing import ClassVar
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    """
+    Base config
+    """
+
+    TESTING: bool = False
+    SECRET_KEY: ClassVar[str | None] = getenv("SECRET_KEY")
+
+
+class DevelopmentConfig(Config):
+    """
+    Development config
+    """
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
+
+
+class TestConfig(Config):
+    """
+    Test config
+    """
+
+    TESTING: bool = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test-project.db"
+
+
+class ProductionConfig(Config):
+    """
+    Production config
+    """
