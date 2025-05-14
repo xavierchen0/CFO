5ebd257574264364a20e9c65abc06528a254529a
Xavier Chen <xaviernehc@gmail.com>
2025-05-14 14:45:35 +0800

    feat: add upload files functionality

--- cfo/static/upload_files_layout.py
+++ cfo/static/upload_files_layout.py
@@ -1,6 +1,30 @@
 from cfo.static.reuse_components import top_upload_files
+from dash import html, dcc
+import dash_bootstrap_components as dbc
 
 upload_files_layout = [
     # Top of page
-    top_upload_files
+    top_upload_files,
+    # Upload
+    dbc.Container(
+        dbc.Row(
+            dcc.Upload(
+                id="upload-data",
+                children=html.Div(
+                    ["Drag and Drop, or ", html.A("Select Files")]
+                ),
+                style={
+                    "height": "300px",
+                    "lineHeight": "300px",
+                    "borderWidth": "1px",
+                    "borderStyle": "dashed",
+                    "borderRadeus": "5px",
+                    "textAlign": "center",
+                },
+            ),
+            justify="center",
+            class_name="mx-3 mt-4",
+        ),
+        fluid=True,
+    ),
 ]
